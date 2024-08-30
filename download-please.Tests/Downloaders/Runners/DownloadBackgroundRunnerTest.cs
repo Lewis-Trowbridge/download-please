a24f94c37c1b4903256980e9c4b07d4c7b163cc6
Lewis Trowbridge <lewbob9927@gmail.com>
2024-08-30 18:07:23 +0100

    Added test for background service

---
+++ download-please.Tests/Downloaders/Runners/DownloadBackgroundRunnerTest.cs
@@ -0,0 +1,27 @@
+﻿using download_please.Downloaders;
+using Downloaders.Runners;
+
+namespace download_please.Tests.Downloaders.Runners
+{
+    public class DownloadBackgroundRunnerTest
+    {
+        public Mock<IDownloader> MockDownloader { get; }
+        public DownloadRequest FakeDownloadRequest { get; } = new DownloadRequest();
+        public string FakeFileUri { get; } = "mockFileUri";
+        public DownloadBackgroundRunner TestRunner { get; }
+
+        public DownloadBackgroundRunnerTest()
+        {
+            MockDownloader = new Mock<IDownloader>();
+            TestRunner = new DownloadBackgroundRunner(MockDownloader.Object, FakeDownloadRequest, FakeFileUri);
+        }
+
+        [Fact]
+        public async Task DownloadBackgroundRunner_WhenCalled_CallsDownloaderWithGivenArguments()
+        {
+            await TestRunner.StartAsync(CancellationToken.None);
+
+            MockDownloader.Verify(x => x.Download(FakeDownloadRequest, FakeFileUri, It.IsAny<CancellationToken>()), Times.Once());
+        }
+    }
+}
